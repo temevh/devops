e2096a259abcb9b3ea4cfc8d5fd973609e5b56bc
temevh <temehama@gmail.com>
2026-09-03 16:22:13 +0300

    trying to fix the /log endpoint output

--- logService.ts
+++ logService.ts
@@ -1,19 +1,35 @@
 import express, { type Express, type Request, type Response } from 'express';
-import fs from "fs"
+import fs from 'fs';
 
 const app: Express = express();
 let counter = 0;
 
-app.get('/log', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
     counter += 1;
-    const timeStamp = new Date()
-    const addToLog = `\r\n${counter},${timeStamp.toISOString()}`
-    fs.appendFile('logs.txt', addToLog, function(err){
+    const timeStamp = new Date();
+    const addToLog = `\r\n${counter},${timeStamp.toISOString()}`;
+    fs.appendFile('logs.txt', addToLog, function (err) {
         if (err) throw err;
-        console.log("Added", addToLog, "to logs")
-    })
-    res.send(timeStamp)
-    res.send('Hello World!');
+        console.log('Added', addToLog, 'to logs');
+    });
+    res.send(timeStamp);
+});
+
+app.get('/log', (req: Request, res: Response) => {
+    fs.readFile('logs.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            res.status(500).send('Error reading logs');
+            return;
+        }
+
+        if (!data) {
+            res.send('No log data found');
+            return;
+        }
+
+        res.type('text/plain').send(data);
+    });
 });
 
 app.listen(3000);
